e48f51ac4ccc8bef8526ae33cd2f5fb5a6c2c158
bobslee <bob@novacode.nl>
2024-03-30 16:03:21 +0100

    [IMP] formio: Remove obsolete model formio.extra.asset

--- formio/models/formio_extra_asset.py
+++
@@ -1,9 +0,0 @@
-# Copyright Nova Code (http://www.novacode.nl)
-# See LICENSE file for full licensing details.
-
-from odoo import models
-
-
-class ExtraAsset(models.Model):
-    _name = 'formio.extra.asset'
-    _description = 'formio.js Extra Asset'
