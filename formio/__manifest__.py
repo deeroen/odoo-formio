c49e978c2d718e30cb99313dda35e78f51c9029d
bobslee <bob@novacode.nl>
2024-06-03 11:16:25 +0200

    [IMP] formio: Improve Form form-view layout. Move submission fields to the right (group)

--- formio/__manifest__.py
+++ formio/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Forms',
     'summary': 'Form Builder for backend, portal, website and embedded forms, to collect any information you need for your business.',
-    'version': '16.0.14.0.1',
+    'version': '16.0.14.0.2',
     'license': 'LGPL-3',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
